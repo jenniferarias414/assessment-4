6089f76d8ffad62597755000b121fbc18481b63e
Jenny Arias <jennyarias@Jennys-Air.attlocal.net>
2023-12-08 13:26:27 -0600

    assessment 4

---
+++ server/controller.js
@@ -0,0 +1,22 @@
+module.exports = {
+
+    getCompliment: (req, res) => {
+        const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
+      
+        // choose random compliment
+        let randomIndex = Math.floor(Math.random() * compliments.length);
+        let randomCompliment = compliments[randomIndex];
+      
+        res.status(200).send(randomCompliment);
+    },
+
+    getFortune: (req, res) => {
+        const fortunes = ["Adventure can be real happiness.", "Change is happening in your life, so go with the flow!", "Do not expect romantic attachments to be strictly logical or rational.", "Go take a rest; you deserve it.", "He who knows he has enough is rich.", "Help! I am being held prisoner in a chinese bakery!"];
+
+        let randomIndex = Math.floor(Math.random() * fortunes.length);
+        let randomFortune = fortunes[randomIndex];
+
+        res.status(200).send(randomFortune);
+    }
+
+}
