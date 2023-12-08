c8d737be84918394ebdd38b06e2af04608ba7a71
Jenny Arias <jennyarias@Jennys-Air.attlocal.net>
2023-12-08 14:55:27 -0600

    fortune btn working

--- client/main.js
+++ client/main.js
@@ -10,7 +10,7 @@ const getCompliment = () => {
 };
 
 const getFortune = () => {
-    axios.get("http://localhost:4000/api/compliment/")
+    axios.get("http://localhost:4000/api/fortune/")
         .then(res => {
             const data = res.data;
             alert(data);
--- server/index.js
+++ server/index.js
@@ -1,15 +1,19 @@
+//----REQUIRE ALL FILES/PKGS-------
 const express = require("express");
 const cors = require("cors");
-
-const app = express();
-
-app.use(cors());
-
-app.use(express.json());
-
 const { getCompliment, getFortune } = require('./controller')
 
-app.get("/api/compliment", getCompliment);
-app.get("/api/compliment", getFortune)
+//------CONFIGURE THE SERVER------
+const app = express();
+app.use(cors());
+app.use(express.json());
 
+
+//------ENDPOINTS-------
+
+
+app.get("/api/compliment", getCompliment);
+app.get("/api/fortune", getFortune)
+
+//--------LISTEN----------
 app.listen(4000, () => console.log("Server running on 4000"));
